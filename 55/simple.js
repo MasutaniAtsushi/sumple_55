ef8afd88df998653cc40c6ddd25e7427f906cc35
MasutaniAtsushi <161193889+MasutaniAtsushi@users.noreply.github.com>
2024-03-04 10:34:34 +0900

    Add files via upload

--- 55/simple.js
+++ 55/simple.js
@@ -1,20 +1,11 @@
-(()=>{
 
-    caliculate = ()=>{
-        const $number = document.getElementById('sentence');
-        let sum = 0
-        for(let index = 1;index <= 10;index++){
-       sum = sum + index;
-    }
-    window.alert(sum);
-
+const $number = document.getElementById('sentence');
+let sum = 0
+for (let index = 1; index <= 10; index++) {
+    sum = sum + index;
 }
-caliculate();
+window.alert(sum);
 
 
 
 
-    
-
-
-})()
